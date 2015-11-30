2a86eebfec794c0fc28e286ae987847d1685ca18
bhrigumalhotra <bhrigu.malhotra@kronos.com>
2015-11-30 05:55:14 -0500

    first commit

---
+++ JavaHelloWorld.java
@@ -0,0 +1,6 @@
+public class JavaHelloWorld {
+	public static void main (String[] args) {
+		System.out.println("Line 1 : Java Hello World");
+		System.out.println("Line 2 : Java Hello again");
+	}
+}
